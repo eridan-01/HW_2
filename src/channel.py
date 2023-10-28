31c6ba18d1a7759d26a6e4cb28510f50fef84bb8
eugene <geymzheka@gmail.com>
2023-10-28 20:12:17 +0500

    added initializers

--- src/channel.py
+++ src/channel.py
@@ -19,6 +19,12 @@ class Channel:
         self.channel = self.youtube.channels().list(
             id=channel_id, part='snippet,statistics'
         ).execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = f'https://www.youtube.com/channel/{self.channel_id}'
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
