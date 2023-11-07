06a0e2ce0289f5d724f28ca75978b804cedbda04
eugene <geymzheka@gmail.com>
2023-11-07 16:20:52 +0500

    added initialization and func print_info()

--- src/playlist.py
+++ src/playlist.py
@@ -1,5 +1,24 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+
 class PlayList:
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, playlist_id):
         self.playlist_id = playlist_id
+        self.playlist_info = self.youtube.playlists().list(id=playlist_id, part='snippet', ).execute()
+        self.playlist_video = self.youtube.playlistItems().list(
+            playlistId=playlist_id,
+            part='snippet',
+            maxResults=50,
+            ).execute()
+        self.title = self.playlist_info['items'][0]['snippet']['title']
+        self.url = f'https://www.youtube.com/playlist?list={self.playlist_id}'
 
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о видео."""
+        print(json.dumps(self.playlist_info, indent=2, ensure_ascii=False))
