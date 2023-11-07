2f9afc723776ca184a7d1604bdf117e3b7173c77
eugene <geymzheka@gmail.com>
2023-11-07 22:04:51 +0500

    added func total_duration

--- src/playlist.py
+++ src/playlist.py
@@ -1,5 +1,7 @@
 import json
 import os
+import datetime
+import isodate
 
 from googleapiclient.discovery import build
 
@@ -10,15 +12,31 @@ class PlayList:
 
     def __init__(self, playlist_id):
         self.playlist_id = playlist_id
-        self.playlist_info = self.youtube.playlists().list(id=playlist_id, part='snippet', ).execute()
-        self.playlist_video = self.youtube.playlistItems().list(
+        self.playlist_info = self.youtube.playlists().list(id=playlist_id, part='contentDetails, snippet', ).execute()
+        self.playlist_videos = self.youtube.playlistItems().list(
             playlistId=playlist_id,
-            part='snippet',
+            part='contentDetails, snippet',
             maxResults=50,
             ).execute()
         self.title = self.playlist_info['items'][0]['snippet']['title']
         self.url = f'https://www.youtube.com/playlist?list={self.playlist_id}'
+        self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
+        self.video_response = self.youtube.videos().list(
+            part='contentDetails,statistics',
+            id=','.join(self.video_ids)
+            ).execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о видео."""
         print(json.dumps(self.playlist_info, indent=2, ensure_ascii=False))
+        print(json.dumps(self.playlist_videos, indent=2, ensure_ascii=False))
+        print(json.dumps(self.video_response, indent=2, ensure_ascii=False))
+
+    @property
+    def total_duration(self):
+        total_duration = datetime.timedelta()
+        for video in self.video_response['items']:
+            iso_8601_duration = video['contentDetails']['duration']
+            duration = isodate.parse_duration(iso_8601_duration)
+            total_duration += datetime.timedelta(seconds=duration.total_seconds())
+        return total_duration
