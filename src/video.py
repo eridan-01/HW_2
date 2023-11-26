40308c9d6f0d3b4ec6bb8ba7d366586140f44ba6
eugene <geymzheka@gmail.com>
2023-11-26 18:09:50 +0500

    added try/except

--- src/video.py
+++ src/video.py
@@ -10,15 +10,23 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id):
-        self.video_id = video_id
-        self.video_response = self.youtube.videos().list(
-            part='snippet,statistics,contentDetails,topicDetails',
-            id=video_id
+        try:
+            self.video_id = video_id
+            self.video_response = self.youtube.videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',
+                id=video_id
             ).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+            self.video_title: str = self.video_response['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            print('Invalid ID')
+            self.video_response = Noneed
+            self.video_title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о видео."""
